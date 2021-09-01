5881ad0074b31d96ca533702585f9dacc09ddc50
Daniele <78617577+DanieleDeleonardis@users.noreply.github.com>
2021-09-01 17:59:13 +0200

    update nav

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,16 @@
 let hamburgerBtn = document.querySelector('#hamburgerBtn');
+let header = document.querySelector('.header');
+let menuMobile = document.querySelector('.header__menu_mobile');
+// let linkMenuMobile = document.querySelector('a[href="#"]');  
 
 hamburgerBtn.addEventListener('click', () => {
-    hamburgerBtn.classList.toggle('open');
+    header.classList.toggle('open');
+    menuMobile.classList.toggle('header__menu_mobile_active');
 })
+
+linkMenuMobile.addEventListener('click', () => {
+    // header.classList.toggle('open');
+    // menuMobile.classList.toggle('header__menu_mobile_active');
+    alert('we')
+})
+
