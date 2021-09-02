f16c76818f852361a25502d9732e95d598ebab61
Daniele <78617577+DanieleDeleonardis@users.noreply.github.com>
2021-09-02 15:16:54 +0200

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,21 @@
 let hamburgerBtn = document.querySelector('#hamburgerBtn');
 let header = document.querySelector('.header');
 let menuMobile = document.querySelector('.header__menu_mobile');
+let body = document.querySelector('body');
 // let linkMenuMobile = document.querySelector('a[href="#"]');  
 
 hamburgerBtn.addEventListener('click', () => {
     header.classList.toggle('open');
     menuMobile.classList.toggle('header__menu_mobile_active');
-    menuMobile.style.transition = "all 0.5s ease"
+    menuMobile.style.transition = "all 0.5s ease-in-out"
 })
 
-linkMenuMobile.addEventListener('click', () => {
-    // header.classList.toggle('open');
-    // menuMobile.classList.toggle('header__menu_mobile_active');
-    alert('we')
-})
+
+menuMobile.addEventListener("click", (e) => {
+    if (menuMobile.contains(e.target)) {
+    header.classList.toggle('open');
+    menuMobile.classList.remove('header__menu_mobile_active');
+    } 
+  });
 
 
