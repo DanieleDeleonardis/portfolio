39777884878298e33a5646584852bf88c15f1990
Daniele <78617577+DanieleDeleonardis@users.noreply.github.com>
2021-09-06 23:40:48 +0200

    update menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 let hamburgerBtn = document.querySelector('#hamburgerBtn');
 let header = document.querySelector('.header');
 let menuMobile = document.querySelector('.header__menu_mobile');
+let menuMobileDisable = document.querySelector('.header__menu_mobile_disable');
 let body = document.querySelector('body');
 
 
@@ -11,10 +12,16 @@ hamburgerBtn.addEventListener('click', () => {
 });
 
 
-menuMobile.addEventListener("click", (e) => {
-    if (menuMobile.contains(e.target)) {
-    header.classList.toggle('open');
-    menuMobile.classList.remove('header__menu_mobile_active');
-    } 
-});
+// menuMobile.addEventListener("click", (e) => {
+//     if (menuMobile.contains(e.target)) {
+//     header.classList.toggle('open');
+//     menuMobile.classList.remove('header__menu_mobile_active');
+//     } 
+// });
 
+body.addEventListener("click", (e) => {
+    if (!e.target.classList.contains('header__menu_mobile_active')) {
+        // menuMobile.classList.remove('header__menu_mobile_active');
+        console.log('we');
+    }
+  });
